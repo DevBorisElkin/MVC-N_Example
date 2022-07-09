4461ef3052da09fd394055716944801bbfd24886
DevBorisElkin <ea.foreverrr@gmail.com>
2022-07-09 12:15:10 +0300

    Basic prep up

---
+++ MVC-N_Example/Comments/Controller/CommentsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MVC-N_Example
+//
+//  Created by test on 09.07.2022.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    @IBOutlet weak var tableView : UITableView!
+
+
+}
+
+extension CommentsViewController : UITableViewDelegate{
+    
+}
+
+extension CommentsViewController : UITableViewDataSource{
+    
+}
+
